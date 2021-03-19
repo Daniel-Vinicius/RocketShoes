b7d0f86fd480960e6b51ce92eed8e1ffec1b2fe7
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-03-19 17:46:33 -0300

    2 testes passando

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { createContext, ReactNode, useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,21 +23,31 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem("@RocketShoes:cart");
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      const storeged = JSON.parse(storagedCart);
+      return [storeged];
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
-    } catch {
-      // TODO
-    }
+      const response = await api.post<Product[]>("/products");
+      const productFilter = response.data.filter(
+        (product) => product.id === productId
+      );
+
+      productFilter.map((product) => {
+        localStorage.setItem(
+          "@RocketShoes:cart",
+          JSON.stringify({ ...cart, product })
+        );
+        setCart([...cart, product]);
+      });
+    } catch {}
   };
 
   const removeProduct = (productId: number) => {
